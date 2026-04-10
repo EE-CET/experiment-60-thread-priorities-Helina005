011e9c1dcc53743e40e7eef4d1c1eaeb2edf192f
vaishnav <vaishnssavkunschuaa@gmail.com>
2026-04-10 20:12:41 +0530

    ThreadPriorityDemo

--- src/ThreadPriorityDemo.java
+++ src/ThreadPriorityDemo.java
@@ -3,32 +3,45 @@
 
 
 
+
+
+
+
+
+
+
 class PriorityThread extends Thread {
     public PriorityThread(String name) {
         super(name);
     }
 
     public void run() {
-        // Just displays the name and priority as per the required output format
         System.out.println(getName() + " Priority: " + getPriority());
     }
 }
 
-public class ThreadPriorityDemo {
+public class ThreadPriorityDemo { 
     public static void main(String[] args) {
-        // Create three threads
         PriorityThread t1 = new PriorityThread("Thread 1");
         PriorityThread t2 = new PriorityThread("Thread 2");
         PriorityThread t3 = new PriorityThread("Thread 3");
 
-        // Set the priorities as required
         t1.setPriority(Thread.MIN_PRIORITY);  // 1
         t2.setPriority(Thread.NORM_PRIORITY); // 5
         t3.setPriority(Thread.MAX_PRIORITY);  // 10
 
-        // Start the threads
-        t1.start();
-        t2.start();
-        t3.start();
+        // Start and Join each thread one by one to force the specific output order
+        try {
+            t1.start();
+            t1.join(); 
+            
+            t2.start();
+            t2.join();
+            
+            t3.start();
+            t3.join();
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
     }
 }
